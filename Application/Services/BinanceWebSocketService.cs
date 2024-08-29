a821767d3643c36f97208682d704beb6aa79a65a
Yovko Garkov <iovko1993@gmail.com>
2024-08-29 23:20:40 +0300

    Create binance web socket service and price data collector

---
+++ Application/Services/BinanceWebSocketService.cs
@@ -0,0 +1,29 @@
+﻿namespace Application.Services
+{
+    #region Usings
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Hosting;
+    #endregion
+
+    public class BinanceWebSocketService : BackgroundService
+    {
+        private readonly IServiceScopeFactory _scopeFactory;
+
+        public BinanceWebSocketService(IServiceScopeFactory scopeFactory)
+        {
+            _scopeFactory = scopeFactory;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                using (var scope = _scopeFactory.CreateScope())
+                {
+                    var priceDataCollector = scope.ServiceProvider.GetRequiredService<PriceDataCollector>();
+                    await priceDataCollector.CollectPriceDataAsync(stoppingToken);
+                }
+            }
+        }
+    }
+}
---
+++ Application/Services/PriceDataCollector.cs
@@ -0,0 +1,75 @@
+﻿namespace Application.Services
+{
+    #region Usings
+    using Data.Context;
+    using Data.Models;
+    using Newtonsoft.Json.Linq;
+    using System.Net.WebSockets;
+    using System.Text;
+    #endregion
+
+    public class PriceDataCollector
+    {
+        private readonly AppDbContext _context;
+
+        public PriceDataCollector(
+            AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task CollectPriceDataAsync(CancellationToken cancellationToken)
+        {
+            using var clientWebSocket = new ClientWebSocket();
+
+            var uri = new Uri("wss://stream.binance.com:9443/ws/btcusdt@trade/adausdt@trade/ethusdt@trade");
+
+            await clientWebSocket.ConnectAsync(uri, cancellationToken);
+
+            var buffer = new byte[1024 * 4];
+
+            while (clientWebSocket.State == WebSocketState.Open && !cancellationToken.IsCancellationRequested)
+            {
+                //await Task.Delay(100, cancellationToken);
+
+                var result = await clientWebSocket.ReceiveAsync(new ArraySegment<byte>(buffer), cancellationToken);
+
+                if (result.MessageType == WebSocketMessageType.Close)
+                {
+                    await clientWebSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, string.Empty, cancellationToken);
+                }
+                else
+                {
+                    var jsonMessage = Encoding.UTF8.GetString(buffer, 0, result.Count);
+                    await ProcessMessageAsync(jsonMessage);
+                }
+            }
+        }
+
+        private async Task ProcessMessageAsync(string jsonMessage)
+        {
+            try
+            {
+                var jObject = JObject.Parse(jsonMessage);
+
+                var symbol = jObject["s"].ToString(); // Symbol (BTCUSDT/ETHUSDT/ADAUSDT)
+                var price = decimal.Parse(jObject["p"].ToString()); // Price
+
+                var priceData = new PriceData
+                {
+                    Symbol = symbol,
+                    Price = price,
+                    Timestamp = DateTime.UtcNow
+                };
+
+                _context.Prices.Add(priceData);
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                // Handle errors (e.g., logging)
+                Console.WriteLine($"Error processing message: {ex.Message}");
+            }
+        }
+    }
+}
