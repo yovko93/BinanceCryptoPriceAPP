696f031e5827df50fb8822ae65b30e04ffa40eff
Yovko Garkov <iovko1993@gmail.com>
2024-09-01 21:03:07 +0300

    Modify console app for the background service

--- CryptoPriceConsoleApp/App.cs
+++ CryptoPriceConsoleApp/App.cs
@@ -1,31 +1,51 @@
-﻿namespace CryptoPriceConsoleApp
+﻿using Application.Services;
+
+namespace CryptoPriceConsoleApp
 {
     public class App
     {
-        private readonly CommandHandler _commandHandler;
+        private CommandHandler _commandHandler;
+        private BinanceWebSocketService _binanceWebSocketService;
 
-        public App(CommandHandler commandHandler)
+        public App(CommandHandler commandHandler, BinanceWebSocketService binanceWebSocketService)
         {
             _commandHandler = commandHandler;
+            _binanceWebSocketService = binanceWebSocketService;
         }
 
         public async Task RunAsync()
         {
             Console.WriteLine("Enter a command (type 'exit' to quit): ");
+            Console.WriteLine();
             Console.WriteLine("24h {symbol} - Returns the average price for the last 24h of data");
-            Console.WriteLine("sma {symbol} {n} {p} {s}(optional)(YYYY-MM-DD) - Return the current Simple Moving average of the symbol's price");
-            Console.WriteLine("symbol options - BTCUSDT / ETHUSDT / ADAUSDT");
-            Console.WriteLine("p options - 1w / 1d / 30m / 5m / 1m");
+            Console.WriteLine("sma {symbol} {n} {p} {s}(optional)(YYYY-MM-DD) - Return the current Simple Moving average of the symbol's price. (symbol-BTCUSDT/ETHUSDT/ADAUSDT | p-1w/1d/30m/5m/1m)");
+            Console.WriteLine();
+            Console.WriteLine("Type 'start' or 'stop' to start/stop the websocket service");
 
             while (true)
             {
                 var input = Console.ReadLine();
+
+                if (input == "start")
+                {
+                    await _binanceWebSocketService.RestartWebSocketAsync();
+                    Console.WriteLine("Web socket started!");
+                    continue;
+                }
+
+                if (input == "stop")
+                {
+                    _binanceWebSocketService.Stop();
+                    Console.WriteLine("Web socket stoped!");
+                    continue;
+                }
 
                 if (string.IsNullOrWhiteSpace(input))
                 {
                     Console.WriteLine("Invalid command.");
                     continue;
                 }
+
 
                 if (input.Trim().ToLower() == "exit")
                     break;
--- CryptoPriceConsoleApp/Program.cs
+++ CryptoPriceConsoleApp/Program.cs
@@ -6,37 +6,49 @@
     using Data.Context;
     using Microsoft.EntityFrameworkCore;
     using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Hosting;
+    using Microsoft.Extensions.Logging;
     #endregion
 
     class Program
     {
         static async Task Main(string[] args)
         {
-            // 1. Set up a service collection and register services
-            var serviceCollection = new ServiceCollection();
-            ConfigureServices(serviceCollection);
-
-            // 2. Build the service provider
-            var serviceProvider = serviceCollection.BuildServiceProvider();
-
-            // 3. Run the application
-            using (var scope = serviceProvider.CreateScope())
+            var host = CreateHostBuilder(args).Build();
+            using (var scope = host.Services.CreateScope())
             {
-                var app = scope.ServiceProvider.GetRequiredService<App>();
+                var commandHandler = scope.ServiceProvider.GetRequiredService<CommandHandler>();
+                var serviceProvider = scope.ServiceProvider
+                          .GetServices<IHostedService>()
+                          .OfType<BinanceWebSocketService>()
+                          .Single();
+
+                var app = new App(commandHandler, serviceProvider);
                 await app.RunAsync();
             }
         }
 
-        private static void ConfigureServices(IServiceCollection services)
-        {
-            services.AddDbContext<AppDbContext>(options =>
-                options.UseNpgsql("Host=localhost;Database=crypto_price_data_db;Username=myappuser;Password=mysecretpassword"));
+        private static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+            .ConfigureLogging(logging =>
+            {
+                logging.ClearProviders();
+                logging.AddConsole();
+                logging.SetMinimumLevel(LogLevel.Warning); // Log only warnings and errors
+            }).ConfigureServices((context, services) =>
+            {
+                services.AddDbContext<AppDbContext>(options =>
+                    options.UseNpgsql("Host=localhost;Database=crypto_price_data_db;Username=myappuser;Password=mysecretpassword"));
 
-            services.AddMemoryCache();
+                services.AddMemoryCache();
 
-            services.AddScoped<IPriceService, PriceService>();
-            services.AddScoped<App>();
-            services.AddScoped<CommandHandler>();
-        }
+                // Register background service
+                services.AddHostedService<BinanceWebSocketService>();
+
+                // Register app service
+                services.AddScoped<IPriceService, PriceService>();
+                services.AddScoped<App>();
+                services.AddScoped<CommandHandler>();
+            });
     }
 }
